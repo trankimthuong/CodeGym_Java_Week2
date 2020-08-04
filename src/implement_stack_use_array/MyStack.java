c73524310f476ee696d79a2a9545c1bb2a8416b2
trankimthuong <thuongtk.bk@gmail.com>
2020-08-04 16:52:38 +0700

    trien khai stack su dung array

---
+++ src/implement_stack_use_array/MyStack.java
@@ -0,0 +1,45 @@
+package implement_stack_use_array;
+
+public class MyStack {
+    public int[] arr;
+    public int size;
+    public int index = 0;
+
+    public MyStack(int size){
+        this.size = size;
+        arr = new int[size];
+    }
+
+    public boolean isEmpty(){
+        if(index == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFull(){
+        if(index == size){
+            return true;
+        }
+        return false;
+    }
+
+    public int size(){
+        return index;
+    }
+
+    public void push(int element){
+        if(isFull()){
+            throw new StackOverflowError("Stack is full");
+        }
+        arr[index] = element;
+        index++;
+    }
+
+    public int pop() throws Exception{
+        if(isEmpty()){
+            throw new Exception("Stack is null");
+        }
+        return arr[--index];
+    }
+}
