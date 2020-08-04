555892dd7bca41623333e14f48fbfd77ef3eed87
trankimthuong <thuongtk.bk@gmail.com>
2020-08-04 10:04:52 +0700

    trien khai queue su dung danh sach lien ket

---
+++ src/implement_queue_use_linkedlist/Node.java
@@ -0,0 +1,11 @@
+package implement_queue_use_linkedlist;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key){
+        this.key = key;
+        this.next = null;
+    }
+}
